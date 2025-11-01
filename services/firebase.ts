a27990ecf3e98f803e01a8af85120669ee134131
aakashshrivastava039-coder <aakashshrivastava039@gmail.com>
2025-11-01 18:48:33 +0530

    Update firebase.ts

--- services/firebase.ts
+++ services/firebase.ts
@@ -3,15 +3,24 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 //Your final, live Firebase configuration is now integrated.
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDs-jG6SzOJBfXpqZ00FSu0NZqf8663gFo",
+//   authDomain: "vibe-oracle.firebaseapp.com",
+//   projectId: "vibe-oracle",
+//   storageBucket: "vibe-oracle.appspot.com",
+//   messagingSenderId: "568173399081",
+//   appId: "1:568173399081:web:7ee00622d8fcbac6b16038",
+//   measurementId: "G-L2GSPZ4TRY"
+// };
 const firebaseConfig = {
   apiKey: "AIzaSyDs-jG6SzOJBfXpqZ00FSu0NZqf8663gFo",
   authDomain: "vibe-oracle.firebaseapp.com",
   projectId: "vibe-oracle",
-  storageBucket: "vibe-oracle.appspot.com",
+  storageBucket: "vibe-oracle.firebasestorage.app",
   messagingSenderId: "568173399081",
   appId: "1:568173399081:web:7ee00622d8fcbac6b16038",
   measurementId: "G-L2GSPZ4TRY"
-};
+}
 // const firebaseConfig = {
 //   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
 //   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
